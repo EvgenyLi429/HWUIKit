a285d54776e1f02d2f39c0447b1ae84b40a6edce
Evgeny Likhachev <EvgenyLi429@gmail.com>
2023-10-01 19:34:18 +0200

    Final Version v1.0

--- HWUIKit/ViewController.swift
+++ HWUIKit/ViewController.swift
@@ -10,56 +10,76 @@ import UIKit
 final class ViewController: UIViewController {
     
     //MARK: - IBOutlets
-    @IBOutlet var monitorClour: UIView!
+    @IBOutlet weak var monitorColor: UIView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
     
-    @IBOutlet var redSlider: UISlider!
-    @IBOutlet var greenSlider: UISlider!
-    @IBOutlet var blueSlider: UISlider!
-    
-    @IBOutlet var redLabel: UILabel!
-    @IBOutlet var greenLabel: UILabel!
-    @IBOutlet var blueLabel: UILabel!
-    
+    //MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         initSliders()
         initSlidersLabels()
-        monitorClour.layer.cornerRadius = 15
+        monitorColor.layer.cornerRadius = 15
+        monitorColor.alpha = 0.0
     }
     
-//MARK: - IBActions
-    @IBAction func sliderAction() {
-        redLabel.text = String(format: "%.1f", redSlider.value)
-        greenLabel.text = String(format: "%.1f", greenSlider.value)
-        blueLabel.text = String(format: "%.1f", blueSlider.value)
+    //MARK: - IBActions
+    @IBAction func greenSliderAction() {
+        greenLabel.text = String(format: "%.2f", CGFloat(greenSlider.value))
+        monitorColor.alpha = CGFloat(greenSlider.value)
+        updateMonitorColor()
+    }
+
+    @IBAction func redSliderAction() {
+        redLabel.text = String(format: "%.2f", CGFloat(redSlider.value))
+        monitorColor.alpha = CGFloat(redSlider.value)
+        updateMonitorColor()
+    }
+        
+    @IBAction func blueSliderAction() {
+        blueLabel.text = String(format: "%.2f", CGFloat(blueSlider.value))
+        monitorColor.alpha = CGFloat(blueSlider.value)
+        updateMonitorColor()
     }
     
+    
+        
+        
     //MARK: - Privates
     private func initSlidersLabels() {
-        redLabel.text = redSlider.value.formatted()
-        greenLabel.text = greenSlider.value.formatted()
-        blueLabel.text = blueSlider.value.formatted()
+        redLabel.text = "0.00"
+        greenLabel.text = "0.00"
+        blueLabel.text = "0.00"
     }
     
     private func initSliders() {
         for slider in [redSlider, greenSlider, blueSlider] {
             guard let slider else {continue}
-            slider.minimumValue = 0.0
-            slider.maximumValue = 1.0
-            slider.maximumTrackTintColor = .systemGray2
-            slider.value = 0.0
+            slider.minimumValue = 0.00
+            slider.maximumValue = 1.00
+            slider.value = 0.00
         }
         redSlider.minimumTrackTintColor = .red
         greenSlider.minimumTrackTintColor = .green
         blueSlider.minimumTrackTintColor = .blue
     }
     
-    
-    
-    
-    
-    
-    
+    private func updateMonitorColor() {
+        let maxAlpha = max(
+            CGFloat(redSlider.value),
+            CGFloat(greenSlider.value),
+            CGFloat(blueSlider.value)
+        )
+        monitorColor.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1.0
+        )
+        monitorColor.alpha = maxAlpha
+    }
 }
